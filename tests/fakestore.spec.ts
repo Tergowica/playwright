76c3b7b25e7cec3265ba5451af2cc4b9322130b1
Michał Łukasik <x16michal@gmail.com>
2026-04-14 20:00:09 +0200

    add scenario price range, and purchase with coupon

--- tests/fakestore.spec.ts
+++ tests/fakestore.spec.ts
@@ -52,3 +52,98 @@ test('remove product in bin', async ({ page }) =>
   await page.getByRole('button', { name: 'Usuń Kurs żeglarski na' }).click();
   await expect(page.getByText('Twój koszyk jest pusty.')).toBeVisible();
 });
+
+
+
+test('purchase with coupon', async ({ page }) => {
+
+
+
+  await page.locator('#menu-item-198').getByRole('link', { name: 'Sklep' }).click();
+  await page.getByRole('link', { name: 'Przejdź do kategorii produktu Yoga i pilates' }).click();
+  await page.getByRole('button', { name: 'Dodaj do koszyka: „Yoga i pilates w Portugalii”' }).click();
+    
+  //   const koszykLink = page.locator('a.added_to_cart');
+  // await expect(koszykLink).toBeVisible();
+  // await koszykLink.click();
+
+  await page.locator('a.added_to_cart').click();
+
+
+  await page.getByRole('textbox', { name: 'Kupon:' }).fill('kwotowy250');
+  await page.getByRole('button', { name: 'Zastosuj kupon' }).click();
+
+  await expect(page.getByText('Kupon został pomyślnie użyty.')).toBeVisible();
+
+
+  await page.getByRole('link', { name: 'Przejdź do płatności ' }).click();
+
+
+  await page.getByRole('textbox', { name: 'BLIK Code' }).click();
+  await page.getByRole('textbox', { name: 'BLIK Code' }).fill('111111');
+  await page.getByRole('checkbox', { name: 'Przeczytałem/am i akceptuję' }).check();
+  await page.getByRole('button', { name: 'Kupuję i płacę' }).click();
+
+
+  await expect(page.getByRole('heading', { name: 'Zamówienie otrzymane' })).toBeVisible();
+});
+
+
+
+
+
+
+
+test('zakres cen', async ({ page }) => {
+
+
+  await page.locator('#menu-item-198').getByRole('link', { name: 'Sklep' }).click();
+  await page.getByRole('link', { name: 'Przejdź do kategorii produktu Windsurfing' }).click();
+
+  await page.locator('#min_price').evaluate((el, value) => {
+  (el as HTMLInputElement).value = value;
+}, '3400');
+
+await page.locator('#max_price').evaluate((el, value) => {
+  (el as HTMLInputElement).value = value;
+}, '3400');
+
+
+// powyższe kroki ustawiają wartości w polach zakresu cen, mimo że są one ukryte, a następnie klikają przycisk filtruj, aby zastosować te wartości
+
+
+
+//  await page.locator('#min_price').fill('3400', { force: true });
+//  await page.locator('#max_price').fill('3400', { force: true });
+
+// to wyżej nie jest krokiem wykonanym, bo elementy są ukryte, więc trzeba użyć JavaScriptu do ustawienia wartości, a następnie kliknąć przycisk filtruj, żeby zastosować te wartości
+
+
+  await page.getByRole('button', { name: 'Filtruj' }).click();
+
+
+
+  await page.getByRole('button', { name: 'Dodaj do koszyka: „Egipt - El' }).click();
+
+
+  await page.locator('a.added_to_cart').click();
+
+
+  await page.getByRole('link', { name: 'Przejdź do płatności ' }).click();
+
+
+  await page.getByRole('textbox', { name: 'BLIK Code' }).click();
+  await page.getByRole('textbox', { name: 'BLIK Code' }).fill('111111');
+  await page.getByRole('checkbox', { name: 'Przeczytałem/am i akceptuję' }).check();
+  await page.getByRole('button', { name: 'Kupuję i płacę' }).click();
+
+
+  await expect(page.getByRole('heading', { name: 'Zamówienie otrzymane' })).toBeVisible();
+
+
+});
+
+
+
+
+
